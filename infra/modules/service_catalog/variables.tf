6e2864168e24b87b2e9e248ba68a83252ec74120
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-03-06 11:07:59 +0530

    Update variables.tf

--- infra/modules/service_catalog/variables.tf
+++ infra/modules/service_catalog/variables.tf
@@ -1,37 +1,37 @@
 variable "portfolio_name" {
-  description = "The name of the Service Catalog portfolio."
+  description = "Name of the Service Catalog portfolio"
   type        = string
 }
 
 variable "portfolio_description" {
-  description = "Description of the Service Catalog portfolio."
+  description = "Portfolio description"
   type        = string
 }
 
 variable "provider_name" {
-  description = "Name of the provider for the Service Catalog."
+  description = "Provider name"
   type        = string
 }
+
 
 variable "launch_role_arn" {
   description = "IAM Role ARN for Service Catalog Launch Constraint"
   type        = string
 }
+
 variable "iam_group_arn" {
-  description = "IAM Group ARN that will be associated with the portfolio."
-  type        = string
+  type = string
 }
 
 variable "products" {
-  description = "A map of products to create in the Service Catalog."
+  description = "List of products"
   type = map(object({
-    product_name        : string
-    product_owner       : string
-    product_description : string
-    support_email       : string
-    support_url         : string
-    artifact_version    : string
-    template_url        : string
-    launch_role_arn     : string
+    name             = string
+    owner            = string
+    description      = string
+    support_email    = string
+    support_url      = string
+    artifact_version = string
+    template_url     = string
   }))
 }
