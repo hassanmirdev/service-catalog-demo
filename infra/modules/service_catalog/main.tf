6785104f19dd1c7fc17e032a22e1a2ca6a433d0e
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-03-06 11:07:20 +0530

    Update main.tf

--- infra/modules/service_catalog/main.tf
+++ infra/modules/service_catalog/main.tf
@@ -4,16 +4,15 @@ resource "aws_servicecatalog_portfolio" "portfolio" {
   provider_name = var.provider_name
 }
 
-# Create Products (EC2 and VPC) dynamically using for_each
 resource "aws_servicecatalog_product" "product" {
   for_each = var.products
 
-  name          = each.value.product_name
-  owner         = each.value.product_owner
-  description   = each.value.product_description
-  type          = "EXTERNAL"
-  support_email = each.value.support_email
-  support_url   = each.value.support_url
+  name                = each.value.name
+  owner               = each.value.owner
+  description         = each.value.description
+  type                = "EXTERNAL"
+  support_email       = each.value.support_email
+  support_url         = each.value.support_url
 
   provisioning_artifact_parameters {
     name         = each.value.artifact_version
@@ -24,7 +23,6 @@ resource "aws_servicecatalog_product" "product" {
   }
 }
 
-# Associate Products with Portfolio dynamically
 resource "aws_servicecatalog_product_portfolio_association" "product_association" {
   for_each = aws_servicecatalog_product.product
 
@@ -32,14 +30,12 @@ resource "aws_servicecatalog_product_portfolio_association" "product_association
   product_id   = each.value.id
 }
 
-# Associate the IAM Group with Portfolio
 resource "aws_servicecatalog_principal_portfolio_association" "portfolio_access_group" {
   portfolio_id   = aws_servicecatalog_portfolio.portfolio.id
   principal_arn  = var.iam_group_arn
   principal_type = "IAM"
 }
 
-# Launch Constraints for each Product dynamically
 resource "aws_servicecatalog_constraint" "launch_constraint" {
   for_each = aws_servicecatalog_product.product
 
@@ -48,8 +44,6 @@ resource "aws_servicecatalog_constraint" "launch_constraint" {
   type         = "LAUNCH"
 
   parameters = jsonencode({
-    "RoleArn" = each.value.launch_role_arn
+    "RoleArn" = var.launch_role_arn
   })
-
-  depends_on = [each.value.launch_role_arn]
 }
