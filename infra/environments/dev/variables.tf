64c6b60e44b6ec0a911551d50b687dc585a275b0
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-03-06 11:04:35 +0530

    Update variables.tf

--- infra/environments/dev/variables.tf
+++ infra/environments/dev/variables.tf
@@ -1,23 +1,3 @@
-variable "portfolio_name" {
-  description = "The name of the Service Catalog portfolio."
-  type        = string
-}
-
-variable "portfolio_description" {
-  description = "Description of the Service Catalog portfolio."
-  type        = string
-}
-
-variable "provider_name" {
-  description = "Name of the provider for the Service Catalog."
-  type        = string
-}
-
-variable "iam_group_arn" {
-  description = "IAM Group ARN that will be associated with the portfolio."
-  type        = string
-}
-
 variable "products" {
   description = "A map of products to create in the Service Catalog."
   type = map(object({
