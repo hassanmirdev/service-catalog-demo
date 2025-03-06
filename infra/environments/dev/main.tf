ae6d1a7f98ae0aac6a7dd6062915be1b8095c3c9
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-03-06 10:38:25 +0530

    Update main.tf

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -15,13 +15,13 @@ module "service_catalog_vpc" {
   portfolio_name        = "VPC Portfolio"
   portfolio_description = "Portfolio for VPC configurations"
   provider_name         = "Network Team (network@example.com)"
-  product_name          = "VPC_Product"
-  product_owner         = "NetworkAdmin"
+  vpc_product_name          = "VPC_Product"
+  vpc_product_owner         = "NetworkAdmin"
   product_description   = "Terraform product to provision a Virtual Private Cloud (VPC)."
-  support_url           = "https://wiki.example.com/network/support"
-  support_email         = "networksupport@example.com"
-  artifact_version      = "1.0"
-  template_url          = "https://servicecatalogterraform.s3.us-east-1.amazonaws.com/vpc-module.tar.gz"
+  vpc_support_url           = "https://wiki.example.com/network/support"
+  vpc_support_email         = "networksupport@example.com"
+  vpc_artifact_version      = "1.0"
+  vpc_template_url          = "https://servicecatalogterraform.s3.us-east-1.amazonaws.com/vpc-module.tar.gz"
   launch_role_arn       = module.iam.iam_role_arn
   iam_group_arn         = module.iam_group.iam_group_arn
 }
