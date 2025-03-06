881c187d3caca1cd1f3a2b4241844e5b8d80a319
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-03-06 10:20:27 +0530

    Update variables.tf

--- infra/modules/service_catalog/variables.tf
+++ infra/modules/service_catalog/variables.tf
@@ -51,3 +51,47 @@ variable "launch_role_arn" {
 variable "iam_group_arn" {
   type = string
 }
+
+
+# Variable for VPC product name
+variable "vpc_product_name" {
+  description = "Service Catalog VPC Product Name"
+  type        = string
+}
+
+# Variable for VPC product owner
+variable "vpc_product_owner" {
+  description = "Owner of the VPC Service Catalog product"
+  type        = string
+}
+
+# Variable for VPC product description
+variable "vpc_product_description" {
+  description = "Description of the VPC Service Catalog product"
+  type        = string
+}
+
+# Support URL for the VPC product
+variable "vpc_support_url" {
+  description = "Support URL for the VPC product"
+  type        = string
+}
+
+# Support email for the VPC product
+variable "vpc_support_email" {
+  description = "Support email for the VPC product"
+  type        = string
+}
+
+# Version for the VPC product artifact
+variable "vpc_artifact_version" {
+  description = "Version of the provisioning artifact for the VPC product"
+  type        = string
+}
+
+# S3 URL for the VPC product Terraform template
+variable "vpc_template_url" {
+  description = "S3 URL for Terraform artifact for the VPC product"
+  type        = string
+}
+
