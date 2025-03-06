b61cb7b702b043160b8cc34ebd95fada04369508
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-03-06 10:47:12 +0530

    Update output.tf

--- infra/modules/service_catalog/output.tf
+++ infra/modules/service_catalog/output.tf
@@ -2,6 +2,6 @@ output "portfolio_id" {
   value = aws_servicecatalog_portfolio.portfolio.id
 }
 
-output "product_id" {
-  value = aws_servicecatalog_product.product.id
+output "product_ids" {
+  value = [for p in aws_servicecatalog_product.product : p.id]
 }
